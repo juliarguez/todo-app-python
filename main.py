d097cd1f803e5a3c9e0b9e58c947fc889f84d949
juliarguez <juliarodriguezg00@gmail.com>
2026-04-15 01:03:06 +0200

    add basic menu structure

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("To-do app")
+tasks = []
+
+while True:
+    print("""
+=== TO-DO APP ===
+1. Ver tareas
+2. Añadir tarea
+3. Eliminar tarea
+4. Completar tarea
+5. Salir
+""")
+
+    option = input("Opción: ")
+
+    if option == "5":
+        break
